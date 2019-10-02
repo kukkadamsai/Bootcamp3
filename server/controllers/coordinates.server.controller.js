c93836b1d7a3236ef4edc4e4bf172aee523adf75
kukkadamsai <kukkadamsai@yahoo.com>
2019-10-02 17:09:13 -0400

    BC3

---
+++ server/controllers/coordinates.server.controller.js
@@ -0,0 +1,42 @@
+var config = require('../config/config'), 
+    request = require('request');
+
+
+
+module.exports = function(req, res, next) {
+  if(req.body.address) {
+      //This code just formats the address so that it doesn't have space and commas using escape characters
+      var addressTemp = req.body.address;
+      var addressTemp2 = addressTemp.toLowerCase();
+      var addressTemp3 = addressTemp2.replace(/\s/g, "%20");
+      var addressTemp4 = addressTemp3.replace(/,/g , "%2C");
+      
+    //Setup your options q and key are provided. Feel free to add others to make the JSON response less verbose and easier to read 
+    var options = { 
+      q: addressTemp4,
+      key: config.openCage.key,  
+    }
+
+    //Setup your request using URL and options - see ? for format
+    request({url: 'https://api.opencagedata.com/geocode/v1/json', qs: options}, function(error, response, body) {
+      if (error) {
+        response.status(400).send(err);
+      }
+      else {
+        var object = JSON.parse(body).results;
+        var geometry = object[0].geometry;
+        var lat = geometry['lat'];
+        var long = geometry['lng'];
+        var coords = {
+          lat: lat,
+          lng: long
+        }
+        req.results = coords;
+        next();
+      }
+    });
+  }
+  else {
+    next();
+  }
+};  
